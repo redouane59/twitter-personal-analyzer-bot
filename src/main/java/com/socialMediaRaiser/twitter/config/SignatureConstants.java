3b1d7d2838eeca69e38518790294497687a8d171
redouane59 <bali.redouane@gmail.com>
2019-04-27 22:33:12 +0200

    cleaning

--- src/main/java/com/socialMediaRaiser/twitter/config/SignatureConstants.java
+++ src/main/java/com/socialMediaRaiser/twitter/config/SignatureConstants.java
@@ -1,4 +1,4 @@
-package com.twitter.config;
+package com.socialMediaRaiser.twitter.config;
 
 import lombok.Data;
 
@@ -6,9 +6,9 @@ import lombok.Data;
 public class SignatureConstants
 {
     public static final String consumerKey = "LiMccelygYuyueOKZLaVk9N1R";
-    public static final String consumerSecret = "CinAcOQG4wjLia8rDQF7EyEKT3xWVpqm4gRCY6jMMsTHJX2Q9j";
+    public static final String consumerSecret = "xxx";
     public static final String accessToken = "92073489-N6BLM48cIKk3X5ya5RiXNPYlShFF1Z1vYRug6rRiv";
-    public static final String secretToken = "1O2IC8WaNrWyJe4n7GUIdTCA6A4QdLJC4Wbk0lbqFhIEc";
+    public static final String secretToken = "xxx";
     public static final String version = "1.0";
     public static final String algorithm = "HmacSHA1";
     public static final String signatureMethod = "HMAC-SHA1";

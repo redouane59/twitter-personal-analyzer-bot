57d30c4136c7f05a52a604213294c8efd9e6c08e
redouane59 <bali.redouane@gmail.com>
2020-06-07 21:17:15 +0200

    fixing test

--- src/test/java/com/socialmediaraiser/twitterbot/integration/BotTest.java
+++ src/test/java/com/socialmediaraiser/twitterbot/integration/BotTest.java
@@ -3,28 +3,27 @@ package com.socialmediaraiser.twitterbot.integration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.socialmediaraiser.twitter.helpers.ConverterHelper;
+import com.socialmediaraiser.twitterbot.GoogleSheetHelper;
 import com.socialmediaraiser.twitterbot.impl.DataArchiveHelper;
+import com.socialmediaraiser.twitterbot.impl.User;
 import com.socialmediaraiser.twitterbot.impl.UserInteraction;
 import io.vavr.collection.Map;
 import java.util.Date;
-import lombok.CustomLog;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-@CustomLog
-public class PersonalAnalyzerBotTest {
-  private Date iniDate;
-  private String            userName;
-  private DataArchiveHelper dataArchiveHelper;
+public class BotTest {
 
+  private Date   iniDate;
+  private String userName;
+  private DataArchiveHelper dataArchiveHelper;
 
   @BeforeAll
   static void init() {
-    LOGGER.info("init");
   }
 
   @Test
-  public void testGetGivenReplies(){
+  public void testAddNewLine(){
     userName = "RedTheOne";
     iniDate = ConverterHelper.dayBeforeNow(30);
     dataArchiveHelper = new DataArchiveHelper(userName, userName.toLowerCase() + "-tweet-history.json", iniDate);
